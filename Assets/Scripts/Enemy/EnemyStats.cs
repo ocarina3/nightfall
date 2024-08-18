be41c43a7a999489c658de10fee71f72d660fe04
Luan Accioly <acciolyluan@gmail.com>
2024-08-18 20:51:43 -0300

    feat: add function to decrease number of alives enemies

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -4,47 +4,54 @@ using UnityEngine;
 
 public class EnemyStats : MonoBehaviour
 {
-    public EnemyScriptableObject enemyData;
+  public EnemyScriptableObject enemyData;
 
-    //Current stats
-    [HideInInspector]
-    public float currentMoveSpeed;
-    [HideInInspector]
-    public float currentHealth;
-    [HideInInspector]
-    public float currentDamage;
+  //Current stats
+  [HideInInspector]
+  public float currentMoveSpeed;
+  [HideInInspector]
+  public float currentHealth;
+  [HideInInspector]
+  public float currentDamage;
 
-    void Awake()
+  void Awake()
+  {
+    //Assign the vaiables
+    currentMoveSpeed = enemyData.MoveSpeed;
+    currentHealth = enemyData.MaxHealth;
+    currentDamage = enemyData.Damage;
+  }
+
+  public void TakeDamage(float dmg)
+  {
+    currentHealth -= dmg;
+
+    if (currentHealth <= 0)
     {
-        //Assign the vaiables
-        currentMoveSpeed = enemyData.MoveSpeed;
-        currentHealth = enemyData.MaxHealth;
-        currentDamage = enemyData.Damage;
+      Kill();
     }
+  }
 
-    public void TakeDamage(float dmg)
+  public void Kill()
+  {
+    Destroy(gameObject);
+  }
+
+
+  void OnCollisionStay2D(Collision2D col)
+  {
+    //Reference the script from the collided collider and deal damage using TakeDamage()
+    if (col.gameObject.CompareTag("Player"))
     {
-        currentHealth -= dmg;
-
-        if (currentHealth <= 0)
-        {
-            Kill();
-        }
+      PlayerStats player = col.gameObject.GetComponent<PlayerStats>();
+      player.TakeDamage(currentDamage);    //Make sure to use currentDamage instead of weaponData.Damage in case any damage multipliers in the future
     }
+  }
 
-    public void Kill()
-    {
-        Destroy(gameObject);
-    }
+  private void OnDestroy()
+  {
+    EnemySpawner es = FindObjectOfType<EnemySpawner>();
+    es.OnEnemyKilled();
+  }
 
-
-    void OnCollisionStay2D(Collision2D col)
-    {
-        //Reference the script from the collided collider and deal damage using TakeDamage()
-        if (col.gameObject.CompareTag("Player"))
-        {
-            PlayerStats player = col.gameObject.GetComponent<PlayerStats>();
-            player.TakeDamage(currentDamage);    //Make sure to use currentDamage instead of weaponData.Damage in case any damage multipliers in the future
-        }
-    }
 }
